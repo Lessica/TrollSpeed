4c2054ef2db3e83c868459f438709772ba2ac32d
82Flex <82flex@gmail.com>
2024-01-21 20:35:09 +0800

    refactor: add unused JetsamHelper
    Signed-off-by: 82Flex <82flex@gmail.com>

--- sources/HUDApp.mm
+++ sources/HUDApp.mm
@@ -1,86 +1,16 @@
-#import <cstddef>
-#import <cstring>
-#import <cstdlib>
-#import <dlfcn.h>
-#import <spawn.h>
 #import <notify.h>
-#import <unistd.h>
-#import <os/log.h>
-#import <rootless.h>
-#import <objc/objc.h>
-#import <sys/param.h>
-#import <sys/sysctl.h>
 #import <mach-o/dyld.h>
 #import <objc/runtime.h>
-#import <CoreFoundation/CoreFoundation.h>
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
 
-
-#pragma mark -
-
-typedef struct __IOHIDEvent *IOHIDEventRef;
-typedef struct __IOHIDNotification *IOHIDNotificationRef;
-typedef struct __IOHIDService *IOHIDServiceRef;
-typedef struct __GSEvent *GSEventRef;
-
-#if __cplusplus
-extern "C" {
-#endif
-void *BKSHIDEventRegisterEventCallback(void (*)(void *, void *, IOHIDServiceRef, IOHIDEventRef));
-void UIApplicationInstantiateSingleton(id aclass);
-void UIApplicationInitialize();
-void BKSDisplayServicesStart();
-void GSInitialize();
-void GSEventInitialize(Boolean registerPurple);
-void GSEventPopRunLoopMode(CFStringRef mode);
-void GSEventPushRunLoopMode(CFStringRef mode);
-void GSEventRegisterEventCallBack(void (*)(GSEventRef));
-#if __cplusplus
-}
-#endif
-
-
-#pragma mark -
-
-@interface UIApplication (Private)
-- (id)_systemAnimationFenceExemptQueue;
-- (void)_accessibilityInit;
-- (void)_enqueueHIDEvent:(IOHIDEventRef)event;
-- (void)__completeAndRunAsPlugin;
-@end
-
-@interface UIEventFetcher : NSObject
-- (void)_receiveHIDEvent:(IOHIDEventRef)arg1;
-@end
-
-@interface AXEventPathInfoRepresentation : NSObject
-@property (assign, nonatomic) unsigned char pathIdentity;
-@end
-
-@interface AXEventHandInfoRepresentation : NSObject
-- (NSArray <AXEventPathInfoRepresentation *> *)paths;
-@end
-
-@interface AXEventRepresentation : NSObject
-@property (nonatomic, readonly) BOOL isTouchDown; 
-@property (nonatomic, readonly) BOOL isMove; 
-@property (nonatomic, readonly) BOOL isChordChange; 
-@property (nonatomic, readonly) BOOL isLift; 
-@property (nonatomic, readonly) BOOL isInRange; 
-@property (nonatomic, readonly) BOOL isInRangeLift; 
-@property (nonatomic, readonly) BOOL isCancel; 
-+ (instancetype)representationWithHIDEvent:(IOHIDEventRef)event hidStreamIdentifier:(NSString *)identifier;
-- (AXEventHandInfoRepresentation *)handInfo;
-- (CGPoint)location;
-@end
-
-
-#pragma mark -
-
+#import "IOKit+SPI.h"
 #import "TSEventFetcher.h"
+#import "BackboardServices.h"
+#import "AXEventRepresentation.h"
+#import "UIApplication+Private.h"
 
-static __used void _HUDEventCallback(void *target, void *refcon, IOHIDServiceRef service, IOHIDEventRef event)
+#define PID_PATH "/var/mobile/Library/Caches/ch.xxtou.hudapp.pid"
+static __used
+void _HUDEventCallback(void *target, void *refcon, IOHIDServiceRef service, IOHIDEventRef event)
 {
     static UIApplication *app = [UIApplication sharedApplication];
 #if DEBUG
@@ -149,43 +79,6 @@ static __used void _HUDEventCallback(void *target, void *refcon, IOHIDServiceRef
         }
     }
 }
-
-
-#pragma mark -
-
-#define PID_PATH "/var/mobile/Library/Caches/ch.xxtou.hudapp.pid"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-#import "libproc.h"
-#import "kern_memorystatus.h"
-#ifdef __cplusplus
-}
-#endif
-
-__unused
-static inline
-void BypassJetsamByProcess(pid_t me, BOOL critical) {
-    int rc; memorystatus_priority_properties_t props = { JETSAM_PRIORITY_CRITICAL, 0 };
-    rc = memorystatus_control(MEMORYSTATUS_CMD_SET_PRIORITY_PROPERTIES, me, 0, &props, sizeof(props));
-    if (critical && rc < 0) { perror ("memorystatus_control"); exit(rc); }
-    rc = memorystatus_control(MEMORYSTATUS_CMD_SET_JETSAM_HIGH_WATER_MARK, me, -1, NULL, 0);
-    if (critical && rc < 0) { perror ("memorystatus_control"); exit(rc); }
-    rc = memorystatus_control(MEMORYSTATUS_CMD_SET_JETSAM_TASK_LIMIT, me, 0, NULL, 0);
-    if (critical && rc < 0) { perror ("memorystatus_control"); exit(rc); }
-    rc = memorystatus_control(MEMORYSTATUS_CMD_SET_PROCESS_IS_MANAGED, me, 0, NULL, 0);
-    if (critical && rc < 0) { perror ("memorystatus_control"); exit(rc); }
-    rc = memorystatus_control(MEMORYSTATUS_CMD_SET_PROCESS_IS_FREEZABLE, me, 0, NULL, 0);
-    if (critical && rc < 0) { perror ("memorystatus_control"); exit(rc); }
-    rc = proc_track_dirty(me, 0);
-    if (critical && rc != 0) { perror("proc_track_dirty"); exit(rc); }
-    os_log_debug(OS_LOG_DEFAULT, "Oh, My Jetsam: %d", me);
-}
-
-
-#pragma mark -
-
 OBJC_EXTERN void SetHUDEnabled(BOOL isEnabled);
 
 int main(int argc, char *argv[])
---
+++ sources/JetsamHelper.h
@@ -0,0 +1,26 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#import "libproc.h"
+#import "kern_memorystatus.h"
+#ifdef __cplusplus
+}
+#endif
+
+static inline __unused
+void BypassJetsamByProcess(pid_t me, BOOL critical) {
+    int rc; memorystatus_priority_properties_t props = { JETSAM_PRIORITY_CRITICAL, 0 };
+    rc = memorystatus_control(MEMORYSTATUS_CMD_SET_PRIORITY_PROPERTIES, me, 0, &props, sizeof(props));
+    if (critical && rc < 0) { perror ("memorystatus_control"); exit(rc); }
+    rc = memorystatus_control(MEMORYSTATUS_CMD_SET_JETSAM_HIGH_WATER_MARK, me, -1, NULL, 0);
+    if (critical && rc < 0) { perror ("memorystatus_control"); exit(rc); }
+    rc = memorystatus_control(MEMORYSTATUS_CMD_SET_JETSAM_TASK_LIMIT, me, 0, NULL, 0);
+    if (critical && rc < 0) { perror ("memorystatus_control"); exit(rc); }
+    rc = memorystatus_control(MEMORYSTATUS_CMD_SET_PROCESS_IS_MANAGED, me, 0, NULL, 0);
+    if (critical && rc < 0) { perror ("memorystatus_control"); exit(rc); }
+    rc = memorystatus_control(MEMORYSTATUS_CMD_SET_PROCESS_IS_FREEZABLE, me, 0, NULL, 0);
+    if (critical && rc < 0) { perror ("memorystatus_control"); exit(rc); }
+    rc = proc_track_dirty(me, 0);
+    if (critical && rc != 0) { perror("proc_track_dirty"); exit(rc); }
+    os_log_debug(OS_LOG_DEFAULT, "Oh, My Jetsam: %d", me);
+}
