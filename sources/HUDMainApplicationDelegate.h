88c4bf6ef3e6648a0caee20d01962dd9b5c17861
82Flex <82flex@gmail.com>
2024-01-21 21:00:59 +0800

    refactor: split HUDMainApplicationDelegate
    Signed-off-by: 82Flex <82flex@gmail.com>

---
+++ sources/HUDMainApplicationDelegate.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface HUDMainApplicationDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic, strong) UIWindow *window;
+@end
---
+++ sources/HUDMainApplicationDelegate.mm
@@ -0,0 +1,64 @@
+#if NO_TROLL
+#error "This target only compiles for real devices."
+#endif
+
+#import <objc/runtime.h>
+
+#import "HUDMainApplicationDelegate.h"
+#import "HUDMainWindow.h"
+#import "HUDRootViewController.h"
+
+#import "SBSAccessibilityWindowHostingController.h"
+#import "UIWindow+Private.h"
+
+@implementation HUDMainApplicationDelegate {
+    HUDRootViewController *_rootViewController;
+    SBSAccessibilityWindowHostingController *_windowHostingController;
+}
+
+- (instancetype)init
+{
+    if (self = [super init])
+    {
+#if DEBUG
+        os_log_debug(OS_LOG_DEFAULT, "- [HUDMainApplicationDelegate init]");
+#endif
+    }
+    return self;
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary <UIApplicationLaunchOptionsKey, id> *)launchOptions
+{
+#if DEBUG
+    os_log_debug(OS_LOG_DEFAULT, "- [HUDMainApplicationDelegate application:%{public}@ didFinishLaunchingWithOptions:%{public}@]", application, launchOptions);
+#endif
+
+    _rootViewController = [[HUDRootViewController alloc] init];
+
+    self.window = [[HUDMainWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self.window setRootViewController:_rootViewController];
+    
+    [self.window setWindowLevel:10000010.0];
+    [self.window setHidden:NO];
+    [self.window makeKeyAndVisible];
+
+    _windowHostingController = [[objc_getClass("SBSAccessibilityWindowHostingController") alloc] init];
+    unsigned int _contextId = [self.window _contextId];
+    double windowLevel = [self.window windowLevel];
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    // [_windowHostingController registerWindowWithContextID:_contextId atLevel:windowLevel];
+    NSMethodSignature *signature = [NSMethodSignature signatureWithObjCTypes:"v@:Id"];
+    NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+    [invocation setTarget:_windowHostingController];
+    [invocation setSelector:NSSelectorFromString(@"registerWindowWithContextID:atLevel:")];
+    [invocation setArgument:&_contextId atIndex:2];
+    [invocation setArgument:&windowLevel atIndex:3];
+    [invocation invoke];
+#pragma clang diagnostic pop
+
+    return YES;
+}
+
+@end
