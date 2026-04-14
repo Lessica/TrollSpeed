f9a0828acd40b5820e2772bf240a1ac58f37b6af
Lessica <82flex@gmail.com>
2026-04-15 05:11:00 +0800

    fix: increase deployment version
    Signed-off-by: Lessica <82flex@gmail.com>

--- build.sh
+++ build.sh
@@ -21,6 +21,7 @@ xcodebuild clean build archive \
 -destination 'generic/platform=iOS' \
 -archivePath TrollSpeed \
 CODE_SIGNING_ALLOWED=NO \
+IPHONEOS_DEPLOYMENT_TARGET=16.0 \
 THEOS="$GITHUB_WORKSPACE/theos" | xcpretty
 
 chmod 0644 Resources/Info.plist
@@ -30,7 +31,6 @@ codesign --remove-signature TrollSpeed.app
 cd - || exit
 cd TrollSpeed.xcarchive/Products || exit
 mv Applications Payload
-rm -f Payload/TrollSpeed.app/Frameworks/libswift_Concurrency.dylib
 ldid -Sentitlements.plist Payload/TrollSpeed.app
 chmod 0644 Payload/TrollSpeed.app/Info.plist
 zip -qr TrollSpeed.tipa Payload
